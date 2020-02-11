10405e69709b75bf73ab31a78babaa755bd9fbd5
liuzhigang <liuzhigang@huobi.com>
2020-02-11 12:03:17 +0800

    自动github pages部署

---
+++ gh-pages.js
@@ -0,0 +1,7 @@
+var ghpages = require('gh-pages');
+ghpages.publish('dist', {
+  branch: 'gh-pages',
+  repo: 'https://github.com/lzg0321/kyc-admin.git'
+}, function(res) {
+  console.log(res);
+});
